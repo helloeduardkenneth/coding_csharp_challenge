4bc7cedcd7dc3437cd2206ade05671f4ebb3af0e
Eduard Kenneth Galuran <eduard.galuran@tondosolutions.com>
2025-06-10 15:16:50 +0800

    CapitalizeString.cs - done

--- CapitalizeString.cs
+++ CapitalizeString.cs
@@ -1,2 +1,22 @@
 // 5. Capitalize String using built-in function - CSharp (Practice)
 
+// Create a function for Capitalization
+public static string CapitalizeSentences(string input)
+{
+    var sentences = input.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+    for (int i = 0; i < sentences.Length; i++)
+    {
+        string sentence = sentences[i].Trim();
+        Console.WriteLine(sentence);
+
+        if (sentence.Length > 0)
+        {
+            sentences[i] = sentence.Substring(0,1).ToUpper() + sentence.Substring(1).ToLower();
+        }
+    }
+    return string.Join(". ", sentences).Trim() + (input.EndsWith(".") ? "." : "");
+}
+
+string str = "THIS IS UPPERCASE. this is lowercase.";
+
+Console.WriteLine(CapitalizeSentences(str));
